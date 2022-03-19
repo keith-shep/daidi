1d4d271ea3fe8daa97987f013283523e9a479032
Keith Shepherdson <kmson7@gmail.com>
2022-03-19 11:12:47 +0800

    add demo for FE calling BE api

--- backend/index.js
+++ backend/index.js
@@ -1,9 +1,15 @@
 const express = require("express");
 const app = express();
+const cors = require("cors");
 const port = 3000;
 
+const returnValue = { content: "hello world" };
+
+app.use(cors());
+
 app.get("/", (req, res) => {
-  res.send("Hello World!");
+  //   res.send("Hello World!");
+  res.json(returnValue);
 });
 
 app.listen(port, () => {
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import axios from "axios";
+
+axios.get("http://localhost:3000/").then((response) => {
+  // alert(response.stringify());
+  alert(JSON.stringify(response.data));
+});
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        {/* <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
@@ -16,7 +22,8 @@ function App() {
           rel="noopener noreferrer"
         >
           Learn React
-        </a>
+        </a> */}
+        hi
       </header>
     </div>
   );
