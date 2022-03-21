bee5c9d7f8fbdd7f23de8d41b7e9ed3476f3593e
Alexander Noronha <alexander@noronha.asia>
2022-03-21 21:52:28 +0800

    created the Round model and basic User model, and initial interaction

--- backend/index.js
+++ backend/index.js
@@ -1,17 +1,34 @@
 const Card = require("./models/Card");
-
+const Round = require("./models/Round");
+const User = require("./models/User");
 const express = require("express");
+
 const app = express();
 const cors = require("cors");
-const port = 3000;
-
-const returnValue = { content: "hello world" };
+const port = 3001;
 
 app.use(cors());
 
 const fourOfSpades = new Card({ suit: "Spades", number: 4 });
-
+const playerOne = new User({name: "John"})
+const roundOne = new Round({ playerTurn: playerOne, typeCombo: "Single"})
+// fourOfSpades.save(function (err) {
+//   if (err) return handleError(err);
+//   // saved!
+// });
+// playerOne.save(function (err) {
+//   if (err) return handleError(err);
+//   // saved!
+// });
+// roundOne.save(function (err) {
+//   if (err) return handleError(err);
+//   // saved!
+// });
 console.log(fourOfSpades);
+console.log(playerOne);
+console.log(roundOne)
+
+const returnValue = { content: "hello world", round: roundOne, playerturn: playerOne, cardPlayed: fourOfSpades };
 
 app.get("/", (req, res) => {
   //   res.send("Hello World!");
---
+++ backend/models/Round.js
@@ -0,0 +1,32 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const RoundSchema = new Schema({
+  won: {
+    type: Boolean,
+    required: true,
+    default: false
+  },
+  playerTurn: {
+    type: Number,
+    type: Schema.Types.ObjectId,
+    ref: 'User',
+    required: true
+  },
+  typeCombo: {
+    type: String,
+    required: true,
+    enum: ["Single", "Pair", "Five-Card"],
+  },
+  winner: {
+    type: Schema.Types.ObjectId,
+    ref: 'User',
+    default: null,
+  },
+});
+
+// This creates our model from the above schema, using mongoose's model method
+const Round = mongoose.model("Round", RoundSchema);
+
+// Export the Article model
+module.exports = Round;
