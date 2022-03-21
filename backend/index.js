bae8c6a26fc34ff3a6f2fc611452e1cc1b617a97
Alexander Noronha <alexander@noronha.asia>
2022-03-21 22:47:06 +0800

    Added a routes catalog and Round Controller

--- backend/index.js
+++ backend/index.js
@@ -1,39 +1,14 @@
 const Card = require("./models/Card");
-const Round = require("./models/Round");
 const User = require("./models/User");
-const express = require("express");
+const catalog = require("./routes/catalog");
 
+const express = require("express");
 const app = express();
 const cors = require("cors");
 const port = 3001;
-
 app.use(cors());
 
-const fourOfSpades = new Card({ suit: "Spades", number: 4 });
-const playerOne = new User({name: "John"})
-const roundOne = new Round({ playerTurn: playerOne, typeCombo: "Single"})
-// fourOfSpades.save(function (err) {
-//   if (err) return handleError(err);
-//   // saved!
-// });
-// playerOne.save(function (err) {
-//   if (err) return handleError(err);
-//   // saved!
-// });
-// roundOne.save(function (err) {
-//   if (err) return handleError(err);
-//   // saved!
-// });
-console.log(fourOfSpades);
-console.log(playerOne);
-console.log(roundOne)
-
-const returnValue = { content: "hello world", round: roundOne, playerturn: playerOne, cardPlayed: fourOfSpades };
-
-app.get("/", (req, res) => {
-  //   res.send("Hello World!");
-  res.json(returnValue);
-});
+app.use('/catalog', catalog)
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
